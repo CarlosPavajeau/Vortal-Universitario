52b78fe2715d7818d64d860ddfb0848c840a1b95
Cantte <cantte098@gmail.com>
2019-06-12 18:55:20 -0500

    Change in the 'Entity' class

--- src/Model/Entity.java
+++ src/Model/Entity.java
@@ -13,9 +13,9 @@ package Model;
  * @author Carlos Pavajeau - Cantte
  */
 
-public abstract class Entity 
+public abstract class Entity implements Comparable<Entity>
 {
-    private String m_code;
+    private long m_code;
     
     /**
      * Initialize a new {@code Entity} with it's code. 
@@ -23,17 +23,49 @@ public abstract class Entity
      * unique and not repeatable by other {@code Entity}.
      * @param The code that is to be assigned to the object
      */
-    public Entity(String code)
+    public Entity(long code)
     {
         m_code = code;
     }
 
     /**
      * This method returns the code that has the object of the Entity class.
-     * @return the code 
+     * @return the code that has the {@code Entity} 
      */
-    public String GetCode()
+    public long GetCode()
     {
         return m_code;
+    }
+
+    /**
+     * This method returns {@code true} if the objects be {@code Entity} or the objects has
+     * equals codes 
+     * @param anObject The object to compare this {@code Entity} against
+     * @return {@code true} if the given object represents a {@code Entity} with
+     *  a equivalent code. {@code false} otherwise
+     */
+    @Override
+    public boolean equals(Object anObject)
+    {
+        if (anObject != null && anObject instanceof Entity)
+        {
+            Entity anEntity = (Entity)anObject;
+            return compareTo(anEntity) == 0;
+        }
+        return false;
+    }
+
+    /**
+     * Compares the codes of two {@code Entity} objects
+     * @param anEntity The object to compare this {@code Entity} against
+     * @return the value {@code 0} if the code of this {@code Entity} is equal to the argument
+     * {@code Entity}, a value less than {@code 0} if the code of this {@code Entity} is 
+     * numerically less than the argument {@code Entity} and a value greater than {@code 0}
+     * if the code of this {@code Entity} is numerically greater than the argument {@code Entity}
+     */
+    @Override
+    public int compareTo(Entity anEntity)
+    {
+        return Long.valueOf(GetCode()).compareTo(Long.valueOf(anEntity.GetCode()));
     }
 }
