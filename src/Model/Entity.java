2e7109bd2e77000d40324397422e25f712744c27
Cantte <cantte098@gmail.com>
2019-06-12 11:12:03 -0500

    Implement the superclass 'Entity'

--- src/Model/Entity.java
+++ src/Model/Entity.java
@@ -1,14 +1,39 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2019-2019 Carlos Pavajeau - Euder Calvo
+ * 
  */
+
 package Model;
 
 /**
- *
- * @author carlos and @ejcalvo33
+ * This is the superclass of most major classes of software. Of course this 
+ * class is abstract. But currently not implemented the abstract methods.
+ * 
+ * @version 1.0
+ * @author Carlos Pavajeau - Cantte
  */
-public class Entity {
+
+public abstract class Entity 
+{
+    private String m_code;
     
+    /**
+     * Initialize a new {@code Entity} with it's code. 
+     * This code identifies the {@code Entity} in the software, also the code is
+     * unique and not repeatable by other {@code Entity}.
+     * @param The code that is to be assigned to the object
+     */
+    public Entity(String code)
+    {
+        m_code = code;
+    }
+
+    /**
+     * This method returns the code that has the object of the Entity class.
+     * @return the code 
+     */
+    public String GetCode()
+    {
+        return m_code;
+    }
 }
