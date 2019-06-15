29fb13dd8ae9d3e6fd596bd19a432577140b3e78
Cantte <cantte098@gmail.com>
2019-06-14 21:19:43 -0500

    Create class 'UnderGraduate'

---
+++ src/Model/UnderGraduate.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2019-2019 Carlos Pavajeau - Euder Calvo
+ * 
+ */
+
+package Model;
+
+import java.util.ArrayList;
+
+/**
+ * This is the class {@code UnderGraduate}, represent a Undergraduate pensum. 
+ * Derive of {@code Pensum}.
+ * @see {@code Pensum}
+ * @version 1.0
+ * @author Carlos Pavajeau - Cantte
+ */
+public class UnderGraduate extends Pensum
+{
+    private String m_titleToGrand;
+
+    /**
+     * Initialize a new {@code UnderGraduate} with these characteristics.
+     * As in {@code UnderGraduate} the code is unique. 
+     * @param code that is to be assigned to the object
+     * @param name it will be the name of the {@code UnderGraduate}
+     * @param description will be the description of the {@code UnderGraduate}
+     * @param campus it will be the campus where {@code UnderGraduate} belongs
+     * @param semesters duration in semesters of {@code UnderGraduate}
+     * @param credits minimum credits required or global credits of the {@code UnderGraduate}
+     * @param subjects subjects that belong or will belong to this {@code UnderGraduate}
+     * @param titleToGrand it will the title to grand
+     */
+    public UnderGraduate(String code, String name, String description, String campus, int semesters, int credits, ArrayList<Subject> subjects, String titleToGrand)
+    {
+        super(code, name, description, campus, semesters, credits, subjects);
+        m_titleToGrand = titleToGrand;
+    }
+
+    /**
+     * This method returns the title to grand that has the object of type {@code UnderGraduate}.
+     * @return the title to grand that has the {@code UnderGraduate}
+     */
+    public String GetTitleToGrand()
+    {
+        return m_titleToGrand;
+    }
+
+    /**
+     * This method sets a new title to grand for this object {@code UnderGraduate}
+     * @param titleToGrand will be a new title to grand of this {@code UnderGraduate}
+     */
+    public void SetTitleToGrand(String titleToGrand)
+    {
+        m_titleToGrand = titleToGrand;
+    }
+}
