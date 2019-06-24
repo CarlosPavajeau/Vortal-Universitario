c3e13677df2b9dbcd8363787dc83a45b6827ea2a
Cantte <cantte098@gmail.com>
2019-06-24 11:19:37 -0500

    Create the 'StudentGroup' class and 'TypeGroup' enum

---
+++ src/Model/StudentGroup.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (C) 2019-2019 Carlos Pavajeau - Euder Calvo
+ * 
+ */
+
+package Model;
+
+import java.util.ArrayList;
+
+/**
+ * This is the class {@code StudentGroup}, represent a Student group. Derive of {@code Entity}.
+ * @see {@code Entity}
+ * @version 1.0
+ * @author Carlos Pavajeau - Cantte
+ */
+public class StudentGroup extends Entity
+{
+    private int m_numGroup;
+    private int m_limitOfStudents;
+    private ArrayList<Student> m_students;
+    private TypeGroup m_type;
+
+    /**
+     * Initialize a new {@code StudentGroup} with these characteristics.
+     * @param code
+     * @param numGroup
+     * @param limitOfStudents
+     * @param typeGroup
+     */
+    public StudentGroup(String code, int numGroup, int limitOfStudents, String typeGroup)
+    {
+        this(code, numGroup, limitOfStudents, typeGroup, new ArrayList<Student>());
+    }
+
+    /**
+     * Initialize a new {@code StudentGroup} with these characteristics.
+     * @param code
+     * @param numGroup
+     * @param limitOfStudents
+     * @param typeGroup
+     * @param students
+     */
+    public StudentGroup(String code, int numGroup, int limitOfStudents, String typeGroup, ArrayList<Student> students)
+    {
+        super(code);
+        m_numGroup = numGroup;
+        m_limitOfStudents = limitOfStudents;
+        m_students = students;
+        SetType(typeGroup);
+    }
+
+    /**
+     * This method returns the number group that this {@code StudentGroup} has. 
+     * @return the number group that this {@code StudentGroup} has.
+     */
+    public int GetNumGroup()
+    {
+        return m_numGroup;
+    }
+
+    /**
+     * This method returns the number group that this {@code StudentGroup} has. 
+     * @return the number group that this {@code StudentGroup} has.
+     */
+    public int GetLimitOfStudents()
+    {
+        return m_limitOfStudents;
+    }
+
+    /**
+     * This method returns students that this {@code StudentGroup} has. 
+     * @return the students that this {@code StudentGroup} has.
+     */
+    public ArrayList<Student> GetStudents()
+    {
+        return m_students;
+    }
+
+    /**
+     * This method returns the type of group that is this {@code StudentGroup}.
+     * @return the the type of group that is this {@code StudentGroup}.
+     */
+    public String GetType()
+    {
+        return m_type.GetType();
+    }
+
+    /**
+     * 
+     * @param limitOfStudents
+     */
+    public void SetLimitOfStudent(int limitOfStudents)
+    {
+        m_limitOfStudents = limitOfStudents;
+    }
+
+    /**
+     * 
+     * @param student
+     * @return
+     */
+    public boolean AddStudent(Student student)
+    {
+        if (m_students.contains(student))
+            return false;
+        return m_students.add(student);
+    }
+
+    /**
+     * 
+     * @param student
+     * @return
+     */
+    public boolean DeleteStudent(Student student)
+    {
+        return m_students.remove(student);
+    }
+
+    /**
+     * 
+     * @param type
+     */
+    private void SetType(String type)
+    {
+        if (TypeGroup.MORNIN.GetType().toUpperCase().equals(type.toUpperCase()))
+            m_type = TypeGroup.MORNIN;
+        else if (TypeGroup.AFTERNOON.GetType().toUpperCase().equals(type.toUpperCase()))
+            m_type = TypeGroup.AFTERNOON;
+        else
+            m_type = TypeGroup.EVENING;
+    }
+
+
+}
---
+++ src/Model/TypeGroup.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2019-2019 Carlos Pavajeau - Euder Calvo
+ * 
+ */
+
+package Model;
+
+/**
+ * This is the enum {@code TypeGroup}, represent one Type group.
+ * @version 1.0
+ * @author Carlos Pavajeau - Cantte
+ */
+public enum TypeGroup
+{
+    MORNIN("Mañana"),
+    AFTERNOON("Tarde"),
+    EVENING("Noche");
+
+    private final String m_type;
+
+    private TypeGroup(String type)
+    {
+        m_type = type;
+    }
+
+    public String GetType()
+    {
+        return m_type;
+    }
+}
