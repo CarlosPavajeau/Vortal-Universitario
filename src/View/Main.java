21aa6618a986eac49c48155fdf4eb313f6e8918f
Cantte <cantte098@gmail.com>
2019-07-01 16:31:07 -0500

    Create a Main class

---
+++ src/View/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2019-2019 Carlos Pavajeau - Euder Calvo
+ * 
+ */
+
+package View;
+
+/**
+ * This is the main class, here the application starts.
+ * @version 1.0
+ * @author Carlos Pavajeau - Cantte
+ */
+
+public class Main
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+}
