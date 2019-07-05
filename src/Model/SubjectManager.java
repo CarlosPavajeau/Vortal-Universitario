bfd523eafa2ee9226e4eea9289b2dcbc8f85f1c9
Cantte <cantte098@gmail.com>
2019-07-05 08:30:56 -0500

    Add 'SubjectManager' interface

---
+++ src/Model/SubjectManager.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2019-2019 Carlos Pavajeau - Euder Calvo
+ * 
+ */
+
+package Model;
+
+/**
+ * This is the interface {@code SubjectManager} for the handling of subjects.
+ * @version 1.0
+ * @author Carlos Pavajeau - Cantte
+ */
+interface SubjectManager
+{
+    boolean AddSubject(Subject subject);
+    boolean RemoveSubject(Subject subject);
+}
