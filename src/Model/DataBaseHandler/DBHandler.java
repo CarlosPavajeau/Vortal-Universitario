87c85bd4027411e19ebc62784228200e67068687
Cantte <cantte098@gmail.com>
2019-07-12 21:28:08 -0500

    Model: Add 'DBHandler' and 'DataHandler' class

---
+++ src/Model/DataBaseHandler/DBHandler.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2019-2019 Carlos Pavajeau - Euder Calvo
+ * 
+ */
+
+package Model.DataBaseHandler;
+
+/**
+ * 
+ */
+interface DBHandler
+{
+
+}
---
+++ src/Model/DataBaseHandler/DataHandler.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2019-2019 Carlos Pavajeau - Euder Calvo
+ * 
+ */
+
+package Model.DataBaseHandler;
+
+import Data.DBConnectionHandler;;
+
+/**
+ * 
+ */
+public abstract class DataHandler implements DBHandler
+{
+    private DBConnectionHandler m_database;
+}
