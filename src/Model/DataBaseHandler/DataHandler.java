bd8242c47dd5418dcf4407fb57792d2c6ccbf7c5
Cantte <cantte098@gmail.com>
2019-07-14 14:28:03 -0500

    DataHandler: Add new methods

--- src/Model/DataBaseHandler/DataHandler.java
+++ src/Model/DataBaseHandler/DataHandler.java
@@ -5,6 +5,8 @@
 
 package Model.DataBaseHandler;
 
+import java.sql.SQLException;
+
 import Data.DBConnectionHandler;;
 
 /**
@@ -13,4 +15,42 @@ import Data.DBConnectionHandler;;
 public abstract class DataHandler implements DBHandler
 {
     private DBConnectionHandler m_database;
+
+    /**
+     * 
+     */
+    public DataHandler()
+    {
+        m_database = new DBConnectionHandler();
+    }
+
+    /**
+     * 
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
+    public void CreateDB() throws SQLException, ClassNotFoundException
+    {
+        m_database.CreateDB();
+    }
+
+    /**
+     * 
+     * @return
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
+    public boolean ConnectWithDB() throws SQLException, ClassNotFoundException
+    {
+        return m_database.ConectWithDB();
+    }
+
+    /**
+     * 
+     * @throws SQLException
+     */
+    public void CloseDB() throws SQLException
+    {
+        m_database.CloseDB();
+    }
 }
