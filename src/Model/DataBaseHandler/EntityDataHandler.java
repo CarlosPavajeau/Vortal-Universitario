47e06a090b29ad7c7f172b6736c2a89fc85959ab
Cantte <cantte098@gmail.com>
2019-07-14 14:34:32 -0500

    Add new class for database manager

---
+++ src/Model/DataBaseHandler/EntityDataHandler.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2019-2019 Carlos Pavajeau - Euder Calvo
+ * 
+ */
+
+package Model.DataBaseHandler;
+
+/**
+ * 
+ */
+public abstract class EntityDataHandler extends DataHandler
+{
+    public EntityDataHandler()
+    {
+        super();
+    }
+}
