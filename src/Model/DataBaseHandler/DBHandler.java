647bf6f8284e5d98009a8ac0385de2987f13f841
Cantte <cantte098@gmail.com>
2019-07-14 14:26:55 -0500

    DBHandler: Add new methods

--- src/Model/DataBaseHandler/DBHandler.java
+++ src/Model/DataBaseHandler/DBHandler.java
@@ -5,10 +5,42 @@
 
 package Model.DataBaseHandler;
 
+import java.sql.SQLException;
+
 /**
  * 
  */
 interface DBHandler
 {
+    String INSERT_COMMAND = "INSERT ";
+    String SELECT_COMMAND = "SELECT ";
+    String UPDATE_COMMAND = "UPDATE ";
+    String DELETE_COMMAND = "DELETE ";
 
+    String FROM_COMMAND = "FROM ";
+    String WHERE_COMMAND = "WHERE ";
+
+    /**
+     * 
+     * @return
+     */
+    boolean Insert(Object anObject) throws SQLException;
+
+    /**
+     * 
+     * @return
+     */
+    Object Select(String objectID) throws SQLException;
+
+    /**
+     * 
+     * @return
+     */
+    boolean Update(Object anObject) throws SQLException;
+
+    /**
+     * 
+     * @return
+     */
+    boolean Delete(Object anObject) throws SQLException;
 }
