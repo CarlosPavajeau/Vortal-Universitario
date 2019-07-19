6d2b5a55d3f4850e4173b3238674c107489c3a27
Cantte <cantte098@gmail.com>
2019-07-19 09:40:23 -0500

    View/FormPanel: Some improvements

--- src/View/FormPanel.java
+++ src/View/FormPanel.java
@@ -7,6 +7,8 @@ package View;
 
 import java.util.ArrayList;
 
+import javax.swing.JLabel;
+
 /**
  * 
  */
@@ -15,6 +17,7 @@ public abstract class FormPanel extends Panel
     private static final long serialVersionUID = -8968908404958389317L;
     
     private ArrayList<TextField> m_fields;
+    private ArrayList<JLabel> m_labels;
 
     public FormPanel()
     {
@@ -25,10 +28,14 @@ public abstract class FormPanel extends Panel
     private void initComponents()
     {
         m_fields = new ArrayList<>();
+        m_labels = new ArrayList<>();
     }
 
-    public void AddTextField(TextField textField)
+    public void AddTextField(TextField textField, String text, int x, int y)
     {
         m_fields.add(textField);
+        m_labels.add(new JLabel(text));
+        textField.setBounds(x, y, textField.getWidth(), textField.getHeight());
+        add(textField);
     }
 }
