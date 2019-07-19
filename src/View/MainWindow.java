deedb9997b5079ab355ba91485fc0b06ea289158
Cantte <cantte098@gmail.com>
2019-07-19 09:42:53 -0500

    View/MainWindow: Some improvements

--- src/View/MainWindow.java
+++ src/View/MainWindow.java
@@ -7,11 +7,11 @@ package View;
 
 import java.awt.Color;
 import java.awt.Frame;
-import java.awt.Rectangle;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 
 /**
@@ -22,19 +22,23 @@ public class MainWindow extends JFrame
     private static final long serialVersionUID = 9040978770256604819L;
 
     List<Panel> m_panels;
+    JPanel m_mainPanel;
 
     public MainWindow() 
     {
+        super();
         initComponents();
     }
 
     private void initComponents() 
     {
         m_panels = new ArrayList<>();
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        m_mainPanel = new JPanel();
+        setContentPane(m_mainPanel);
+        m_mainPanel.setBackground(Color.WHITE);
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Vortal Universitario");
         setBackground(Color.WHITE);
-        setBounds(new Rectangle(0, 0, 0, 0));
         setExtendedState(Frame.MAXIMIZED_BOTH);
         setUndecorated(true);
     }
