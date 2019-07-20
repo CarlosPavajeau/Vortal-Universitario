a7289371bf682af1a7d06f814faf84c21fce5915
Cantte <cantte098@gmail.com>
2019-07-20 18:56:08 -0500

    View/FormPanel: Validation of fields implemented, in addition to the management of a title.

--- src/View/FormPanel.java
+++ src/View/FormPanel.java
@@ -5,7 +5,9 @@
 
 package View;
 
+import java.awt.Font;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JLabel;
 
@@ -16,12 +18,14 @@ public abstract class FormPanel extends Panel
 {
     private static final long serialVersionUID = -8968908404958389317L;
     
-    private ArrayList<TextField> m_fields;
-    private ArrayList<JLabel> m_labels;
+    private List<TextField> m_fields;
+    private List<JLabel> m_labels;
+    private final String m_title;
 
-    public FormPanel()
+    public FormPanel(String title)
     {
         super();
+        m_title = title;
         initComponents();
     }
 
@@ -29,13 +33,43 @@ public abstract class FormPanel extends Panel
     {
         m_fields = new ArrayList<>();
         m_labels = new ArrayList<>();
+        SetTitle();
+    }
+
+    public List<TextField> GetTextFields()
+    {
+        return m_fields;
     }
 
     public void AddTextField(TextField textField, String text, int x, int y)
     {
-        m_fields.add(textField);
-        m_labels.add(new JLabel(text));
-        textField.setBounds(x, y, textField.getWidth(), textField.getHeight());
+        JLabel ltext = new JLabel(text);
+        ltext.setFont(new Font("Microsoft Sans Serif", 0, 16));
+        ltext.setBounds(x, y, 120, 50);
+        textField.setBounds(x + ltext.getWidth(), y, textField.getWidth(), textField.getHeight());
+        add(ltext);
         add(textField);
+        m_labels.add(ltext);
+        m_fields.add(textField);
+    }
+
+    public boolean ValidateFields()
+    {
+        for (TextField textField : GetTextFields())
+            if (!textField.IsValidField())
+                return false;
+        return true;
+    }
+
+    public abstract void InitFields();
+
+    private void SetTitle()
+    {
+        JLabel title = new JLabel(m_title);
+        title.setFont(new Font("Microsoft Sans Serif", 0, 25));
+        title.setBounds(0, 5, 700, 100);
+        int x = ((getHeight() - title.getHeight() - getInsets().top - getInsets().bottom) / 2);
+        title.setBounds(x, 5, 700, 100);
+        add(title);
     }
 }
