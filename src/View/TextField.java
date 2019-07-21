2310fd4840d7aeff2f9e2d7a2d3983234f14d162
Cantte <cantte098@gmail.com>
2019-07-21 10:48:44 -0500

    View/TextField: Clean class

--- src/View/TextField.java
+++ src/View/TextField.java
@@ -5,27 +5,12 @@
 
 package View;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.event.FocusAdapter;
-import java.awt.event.FocusEvent;
-
-import javax.swing.JTextField;
-
 /**
  * 
  */
-public class TextField extends JTextField
+public class TextField extends Field
 {
     private static final long serialVersionUID = -1322371677583567319L;
-
-    public static final int OBLIGATORY_FIELD = 1;
-    public static final int NUMERIC_FIELD = 2;
-    public static final int ALPHA_FIELD = 4;
-    
-    private final String m_defaultText;
-    private final int m_typeField;
 
     /**
      * 
@@ -36,102 +21,6 @@ public class TextField extends JTextField
      */
     public TextField(int width, int height, String defaultText, int typeField)
     {
-        m_defaultText = defaultText;
-        m_typeField = typeField;
-        setFont(new Font("Microsoft Sans Serif", 0, 16));
-        setForeground(Color.GRAY);
-        setPreferredSize(new Dimension(width, height));
-        setSize(new Dimension(width, height));
-        setHorizontalAlignment(JTextField.CENTER);
-        setText(m_defaultText);
-        setBorder(new RoundRectBorder());
-        addFocusListener(new FocusAdapter() 
-        {
-            public void focusGained(FocusEvent evt) 
-            {
-                TextFieldFocusGained(evt);
-            }
-
-            public void focusLost(FocusEvent evt)
-            {
-                TextFieldFocusLost(evt);
-            }
-        });
-    }
-
-    /**
-     * 
-     * @return
-     */
-    public boolean IsValidField()
-    {
-        switch (m_typeField)
-        {
-            case TextField.OBLIGATORY_FIELD: 
-                return ValidateObligatoryField();
-            case TextField.ALPHA_FIELD: 
-                return ValidateAlphaField();
-            case TextField.NUMERIC_FIELD: 
-                return ValidateNumericField();
-            case TextField.ALPHA_FIELD + TextField.OBLIGATORY_FIELD: 
-                return ValidateObligatoryField() && ValidateAlphaField();
-            case TextField.NUMERIC_FIELD + TextField.OBLIGATORY_FIELD: 
-                return ValidateObligatoryField() && ValidateNumericField();
-            default:
-                return true;
-        }
-    }
-
-    /**
-     * 
-     * @return
-     */
-    private boolean ValidateObligatoryField()
-    {
-        return !(getText().isEmpty() || getText().equals(m_defaultText));
-    }
-
-    /**
-     * 
-     * @return
-     */
-    private boolean ValidateAlphaField()
-    {
-        return getText().matches("[a-zA-Z]+");
-    }
-
-    /**
-     * 
-     * @return
-     */
-    private boolean ValidateNumericField()
-    {
-        return getText().matches("(\\d+)");
-    }
-
-    /**
-     * 
-     * @param evt
-     */
-    private void TextFieldFocusGained(FocusEvent evt) 
-    {
-        if (getText().equals(m_defaultText))
-        {
-            setForeground(Color.BLACK);
-            setText("");
-        }
-    }
-
-    /**
-     * 
-     * @param evt
-     */
-    private void TextFieldFocusLost(FocusEvent evt)
-    {
-        if (getText().equals(""))
-        {
-            setForeground(Color.GRAY);
-            setText(m_defaultText);
-        }
+        super(width, height, defaultText, typeField);
     }
 }
