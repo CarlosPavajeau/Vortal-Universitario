7fc19e407d581173f13f6ac739f341161bca346e
Cantte <cantte098@gmail.com>
2019-07-23 11:53:37 -0500

    View: Some improvements for 'TypeButton' enum and 'Button' class

--- src/View/TypeButton.java
+++ src/View/TypeButton.java
@@ -5,6 +5,9 @@ public enum TypeButton
     /**Standard buttons */
     BUTTON_LOGIN("Button_Login", "", TypeButton.STANDARD_BUTTON),
     BUTTON_REGISTER("Button_Register", "", TypeButton.STANDARD_BUTTON),
+    BUTTON_EXIT("Button_Exit", "", TypeButton.STANDARD_BUTTON),
+    BUTTON_MATRICULATE("Button_Matriculate", "", TypeButton.STANDARD_BUTTON),
+    BUTTON_LOGOUT("Button_LogOut", "", TypeButton.STANDARD_BUTTON),
     /**50x50 buttons */
     BUTTON_ICON_DANGER("Icon_Danger", "", TypeButton.BUTTON_WITH_SIZE_50x50),
     BUTTON_ICON_HELP("Icon_Help", "", TypeButton.BUTTON_WITH_SIZE_50x50),
@@ -15,11 +18,11 @@ public enum TypeButton
     BUTTON_ICON_WARNING("Icon_Warning", "", TypeButton.BUTTON_WITH_SIZE_50x50),
     BUTTON_ICON_MORTARBOARD("Icon_Mortarboard", "", TypeButton.BUTTON_WITH_SIZE_50x50),
     /**100x100 buttons */
-    BUTTON_ICON_ACCESS_DATA_MANAGER("Icon_AccessDataManager", "", TypeButton.BUTTON_WITH_SIZE_100x100),
+    BUTTON_ICON_ACCESS_DATA_MANAGER("Icon_AccessDataManager", "Gestión de datos de acceso", TypeButton.BUTTON_WITH_SIZE_100x100),
     BUTTON_ICON_ADD_OR_DELETE_STUDENT("Icon_Add-DeleteStudent", "", TypeButton.BUTTON_WITH_SIZE_100x100),
     BUTTON_ICON_ADD_SUBJECT("Icon_AddSubject", "Añadir asignatura", TypeButton.BUTTON_WITH_SIZE_100x100),
     BUTTON_ICON_CURRENT_PROGRESS("Icon_CurrentProgress", "", TypeButton.BUTTON_WITH_SIZE_100x100),
-    BUTTON_ICON_DATA_MANAGER("Icon_DataManager", "", TypeButton.BUTTON_WITH_SIZE_100x100),
+    BUTTON_ICON_DATA_MANAGER("Icon_DataManager", "Gestión de datos", TypeButton.BUTTON_WITH_SIZE_100x100),
     BUTTON_ICON_DELETE_SUBJECT("Icon_DeleteSubject", "", TypeButton.BUTTON_WITH_SIZE_100x100),
     BUTTON_ICON_PERSON("Icon_Person", "", TypeButton.BUTTON_WITH_SIZE_100x100),
     BUTTON_ICON_PERSONAL_DATA_MANAGER("Icon_PersonalDataManager", "", TypeButton.BUTTON_WITH_SIZE_100x100),
@@ -27,7 +30,12 @@ public enum TypeButton
     BUTTON_ICON_SUBJECT_HANDLER("Icon_SubjectHandler", "", TypeButton.BUTTON_WITH_SIZE_100x100),
     BUTTON_ICON_VIEW_PROGRESS("Icon_ViewProgress", "", TypeButton.BUTTON_WITH_SIZE_100x100),
     BUTTON_ICON_VIEW_QUALIFICATIONS("Icon_ViewQualifications", "", TypeButton.BUTTON_WITH_SIZE_100x100),
-    BUTTON_ICON_VIEW_STUDENT_SUBJECTS("Icon_ViewStudentSubjects", "", TypeButton.BUTTON_WITH_SIZE_100x100);
+    BUTTON_ICON_VIEW_STUDENT_SUBJECTS("Icon_ViewStudentSubjects", "", TypeButton.BUTTON_WITH_SIZE_100x100),
+    /**200x200 buttons */
+    BUTTON_ICON_I_AM_ADMIN("I'm_Admin", "", TypeButton.BUTTON_WITH_SIZE_200x200),
+    BUTTON_ICON_I_AM_PROFESSOR("I'm_Professor", "", TypeButton.BUTTON_WITH_SIZE_200x200),
+    BUTTON_ICON_I_AM_STUDENT("I'm_Student", "", TypeButton.BUTTON_WITH_SIZE_200x200)
+    ;
 
     private final String m_src;
     private final String m_rolloverSrc;
@@ -38,10 +46,12 @@ public enum TypeButton
     private static final String STANDARD_BUTTONS_SRC = "/View/Assets/Buttons/";
     private static final String BUTTONS_WITH_ICONS100x100_SRC = "/View/Assets/Icons/100x100/";
     private static final String BUTTONS_WITH_ICONS50x50_SRC = "/View/Assets/Icons/50x50/";
+    private static final String BUTTONS_WITH_ICONS200x200_SRC = "/View/Assets/Icons/200x200/";
 
     private static final int STANDARD_BUTTON = 0;
     private static final int BUTTON_WITH_SIZE_50x50 = 1;
     private static final int BUTTON_WITH_SIZE_100x100 = 2;
+    private static final int BUTTON_WITH_SIZE_200x200 = 3;
 
     private static final int STANDARD_BUTTON_SIZE_WIDTH = 200; 
     private static final int STANDARD_BUTTON_SIZE_HEIGHT = 50;
@@ -51,6 +61,9 @@ public enum TypeButton
 
     private static final int BUTTONS_WITH_ICONS50x50_WIDTH = 50;
     private static final int BUTTONS_WITH_ICONS50x50_HEIGHT = 70;
+
+    private static final int BUTTONS_WITH_ICONS200x200_WIDTH = 200;
+    private static final int BUTTONS_WITH_ICONS200x200_HEIGHT = 220;
 
     private static final String BUTTON_EXT = ".png";
     private static final String BUTTON_ROLLOVER_SUFFIX = "MouseOn";
@@ -82,7 +95,7 @@ public enum TypeButton
             m_width = BUTTONS_WITH_ICONS50x50_WIDTH;
             m_heigth = BUTTONS_WITH_ICONS50x50_HEIGHT;
         }
-        else
+        else if (typeButton == TypeButton.BUTTON_WITH_SIZE_100x100)
         {
             m_src = BUTTONS_WITH_ICONS100x100_SRC + buttonName + BUTTON_EXT;
 
@@ -94,6 +107,18 @@ public enum TypeButton
             m_width = BUTTONS_WITH_ICONS100x100_WIDTH;
             m_heigth = BUTTONS_WITH_ICONS100x100_HEIGHT;
         } 
+        else
+        {
+            m_src = BUTTONS_WITH_ICONS200x200_SRC + buttonName + BUTTON_EXT;
+
+            m_rolloverSrc = BUTTONS_WITH_ICONS200x200_SRC + buttonName + 
+                            BUTTON_ROLLOVER_SUFFIX + BUTTON_EXT;
+            m_pressedSrc = BUTTONS_WITH_ICONS200x200_SRC + buttonName + 
+                            BUTTON_PRESSED_SUFFIX + BUTTON_EXT;
+
+            m_width = BUTTONS_WITH_ICONS200x200_WIDTH;
+            m_heigth = BUTTONS_WITH_ICONS200x200_HEIGHT;
+        }
         m_text = tex;
     }
 
