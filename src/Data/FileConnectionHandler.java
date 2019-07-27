5da302d34e70255d989f1c763b408d5c906fe73c
Cantte <cantte098@gmail.com>
2019-07-27 17:19:56 -0500

    Data: Class 'FileConnectionHandler' cleaner and tested

--- src/Data/FileConnectionHandler.java
+++ src/Data/FileConnectionHandler.java
@@ -13,21 +13,72 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
 
 /**
  * 
  */
-public class FileConnectionHandler implements DataConnection 
+public class FileConnectionHandler implements DataConnection, Serializable 
 {
+    private static final long serialVersionUID = -7926699142824489196L;
+
     private File m_file;
-    private FileOutputStream m_fileOutputStream;
-    private FileInputStream m_fileInputStream;
-    private ObjectOutputStream m_objectOutputStream;
-    private ObjectInputStream m_objectInputStream;
+    private Record m_records;
+
+    private class Record implements Serializable
+    {
+        private static final long serialVersionUID = -8020776646032448921L;
+        private ArrayList<Object> m_record;
+
+        public Record()
+        {
+            m_record = new ArrayList<>();
+        }
+        
+        public boolean AddRecord(Object anObject)
+        {
+            return m_record.add(anObject);
+        }
+
+        public boolean UpdateRecord(Object anObject)
+        {
+            if (m_record.contains(anObject))
+            {
+                m_record.remove(anObject);
+                return m_record.add(anObject);
+            }
+            return false;
+        }
+
+        public Object SelectRecord(Object anObject)
+        {
+            try
+            {
+                return m_record.get(m_record.indexOf(anObject));
+            }
+            catch(IndexOutOfBoundsException exception)
+            {
+                return null;
+            } 
+        }
+
+        public boolean RemoveRecord(Object anObject)
+        {
+            return m_record.remove(anObject);
+        }
+
+        public boolean isEmpty()
+        {
+            return m_record.isEmpty();
+        }
+    }
+
 
     public FileConnectionHandler(String fileName) throws FileNotFoundException
     {
         m_file = new File(fileName);
+        m_records = new Record();
     }
 
     @Override
@@ -35,7 +86,8 @@ public class FileConnectionHandler implements DataConnection
     {
         if (!IsItRegistered(anObject))
         {
-            m_objectOutputStream.writeObject(anObject);
+            m_records.AddRecord(anObject);
+            WriteRecords();
             return true;
         }
         return false;
@@ -50,65 +102,18 @@ public class FileConnectionHandler implements DataConnection
     @Override
     public boolean Update(Object anObject) throws IOException, ClassNotFoundException 
     {
-        File tempFile = new File("temp.temp");
-        FileOutputStream tempFileOutput = new FileOutputStream(tempFile);
-        ObjectOutputStream tempOutput = new ObjectOutputStream(tempFileOutput);
-        Object otherObject = null;
-
-        otherObject = m_objectInputStream.readObject();
-        while (true)
-        {
-            try
-            {
-                if (anObject.equals(otherObject))
-                {
-                    tempOutput.writeObject(anObject);
-                }
-                else
-                    tempOutput.writeObject(otherObject);
-                otherObject = m_objectInputStream.readObject();
-
-            } catch (EOFException exception)
-            {
-                break;
-            }
-        }
-
-        tempOutput.close();
-        tempFile.renameTo(m_file);
-
+        m_records.UpdateRecord(anObject);
+        WriteRecords();
         return true;
     }
 
     @Override
     public boolean Delete(Object anObject) throws IOException, ClassNotFoundException 
     {
-        File tempFile = new File("temp.temp");
-        FileOutputStream tempFileOutput = new FileOutputStream(tempFile);
-        ObjectOutputStream tempOutput = new ObjectOutputStream(tempFileOutput);
-        Object otherObject = null;
-
-        otherObject = m_objectInputStream.readObject();
-        while (true)
-        {
-            try
-            {
-                if (!anObject.equals(otherObject))
-                {
-                    tempOutput.writeObject(otherObject);
-                }
-                otherObject = m_objectInputStream.readObject();
-
-            } catch (EOFException exception)
-            {
-                break;
-            }
-        }
-
-        tempOutput.close();
-        tempFile.renameTo(m_file);
-
-        return true;
+        boolean result = m_records.RemoveRecord(anObject);
+        if (result)
+            WriteRecords();
+        return result;
     }
 
     @Override
@@ -120,53 +125,77 @@ public class FileConnectionHandler implements DataConnection
     @Override
     public boolean ThereIsRecords() throws IOException 
     {
-        return false;
+        return !m_records.isEmpty();
 	}
 
     @Override
     public void CreateDataConnection() throws ClassNotFoundException, IOException 
     {
-        m_fileOutputStream = new FileOutputStream(m_file);
-        m_fileInputStream = new FileInputStream(m_file);
+        if (!m_file.exists())
+        {
+            
+            try (FileOutputStream file = new FileOutputStream(m_file))
+            {
+            } 
+            catch (IOException exception)
+            {
+                throw exception;
+            }
+        }
     }
 
     @Override
     public void CloseDataConnection() throws IOException 
     {
-        if (m_fileOutputStream != null)
-            m_fileOutputStream.close();
-        if (m_fileInputStream != null)
-            m_fileInputStream.close();
-        if (m_objectOutputStream != null)
-            m_objectOutputStream.close();
-        if (m_fileInputStream != null)
-            m_objectInputStream.close();
+
     }
 
     @Override
-    public boolean ConnectWithData() throws IOException 
+    public boolean ConnectWithData() throws IOException, ClassNotFoundException 
     {
-        m_objectOutputStream = new ObjectOutputStream(m_fileOutputStream);
-        m_objectInputStream = new ObjectInputStream(m_fileInputStream);
-        return true;
+        boolean res = true;
+
+        try
+        {
+            ReadRecords();
+        }
+        catch (EOFException | FileNotFoundException exception)
+        {
+            res = false;
+        }
+
+        return res;
     }
     
     private Object SearchObject(Object anObject) throws IOException, ClassNotFoundException
     {
-        Object otherObject = null;
+        return m_records.SelectRecord(anObject);
+    }
 
-        try
+    private void ReadRecords() throws ClassNotFoundException, IOException
+    {
+        try(FileInputStream fileInputStream = new FileInputStream(m_file);
+            ObjectInputStream inputStream = new ObjectInputStream(fileInputStream))
         {
-            otherObject = m_objectInputStream.readObject();
-            while (!otherObject.equals(anObject))
-                otherObject = m_objectInputStream.readObject();
-
-        }catch(EOFException exception)
+            m_records = (Record)inputStream.readObject();
+        } 
+        catch (IOException exception)
         {
-            return null;
+            throw exception;
         }
-        
-        return otherObject;
+    }
+
+    private void WriteRecords() throws IOException
+    {
+        try(FileOutputStream fileOutputStream = new FileOutputStream(m_file); 
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream))
+        {
+            objectOutputStream.writeObject(m_records);
+        }
+        catch (IOException exception)
+        {
+            throw exception;
+        }
     }
 
 }
