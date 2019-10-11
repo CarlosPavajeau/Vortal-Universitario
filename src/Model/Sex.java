862a4eea1335957f686257601812992a02963077
Cantte <cantte098@gmail.com>
2019-10-11 15:39:06 -0500

    Remove unnecessary comments

--- src/Model/Sex.java
+++ src/Model/Sex.java
@@ -7,11 +7,6 @@ package Model;
 
 import java.io.Serializable;
 
-/**
- * This is the enum {@code Sex}, represent one sex.
- * @version 1.0
- * @author Carlos Pavajeau - Cantte
- */
 public enum Sex implements Serializable
 {
     MALE("Masculino"),
@@ -20,10 +15,6 @@ public enum Sex implements Serializable
 
     private final String sex;
 
-    /**
-     * Initialize a new {@code Sex} 
-     * @param sex will be a new sex.
-     */
     private Sex(String _sex)
     {
         sex = _sex;
