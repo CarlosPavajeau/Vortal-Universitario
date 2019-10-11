4e37d8d740a32f4f57690136dbadc6eb72594904
Cantte <cantte098@gmail.com>
2019-10-11 15:14:28 -0500

    View/LabelContants: Add new constants

--- src/View/LabelConstants.java
+++ src/View/LabelConstants.java
@@ -10,4 +10,7 @@ public interface LabelConstants
     byte DEFAULT_FONT_SIZE = 16;
     int DEFAULT_LABEL_WIDTH = 300;
     int DEFAULT_LABEL_HEIGHT = 25;
+
+    int LABEL_TITLE_WIDTH = 700;
+    int LABEL_TITLE_HEIGHT = 100;
 }
