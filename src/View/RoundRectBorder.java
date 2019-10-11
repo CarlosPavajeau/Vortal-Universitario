401c9dc9ac8723ab5d201ee130e81d12e8019343
Carlos Pavajeau <cantte098@gmail.com>
2019-10-11 18:14:05 -0500

    View: Edge handling improvements

--- src/View/RoundRectBorder.java
+++ src/View/RoundRectBorder.java
@@ -18,45 +18,64 @@ import java.awt.geom.RoundRectangle2D;
 
 import javax.swing.border.AbstractBorder;
 
-/**
- * 
- */
 public class RoundRectBorder extends AbstractBorder
 {
     private static final long serialVersionUID = 1554576986093645747L;
     private static final Color ALPHA_ZERO = new Color(0x0, true);
 
-    @Override
-    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height)
-    {
-        Graphics2D g2 = (Graphics2D)g.create();
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        Shape border = getBorderShape(x, y, width - 1, height - 1);
-        g2.setPaint(ALPHA_ZERO);
-        Area corner = new Area(new Rectangle2D.Double(x, y, width, height));
-        corner.subtract(new Area(border));
-        g2.fillRoundRect(x, y, width - 1, height - 1, 5, 5);
-        g2.setPaint(Color.GRAY);
-        g2.drawRoundRect(x, y, width - 1, height - 1, 5, 5);
-        g2.dispose();
-    }
+    private static final int TOP = 4;
+    private static final int LEFT = 8;
+    private static final int BOTTOM = 4;
+    private static final int RIGHT = 8;
+
+    private static final Insets BORDER_INSETS = new Insets(TOP, LEFT, BOTTOM, RIGHT);
     
-    public Shape getBorderShape(int x, int y, int w, int h) 
+    public static final RoundRectBorder GRAY_BORDER = new RoundRectBorder();
+    public static final RoundRectBorder BLUE_BORDER = new RoundRectBorder(Color.BLUE);
+    public static final RoundRectBorder RED_BORDER = new RoundRectBorder(Color.RED);
+
+    private final Color _color;
+
+    public RoundRectBorder()
     {
-        int r = h; //h / 2;
-        return new RoundRectangle2D.Double(x, y, w, h, r, r);
+        _color = Color.GRAY;
     }
-    
-    @Override 
-    public Insets getBorderInsets(Component c) 
+
+    public RoundRectBorder(Color color)
     {
-        return new Insets(4, 8, 4, 8);
+        _color = color;
     }
 
     @Override
-    public Insets getBorderInsets(Component c, Insets insets) 
+    public void paintBorder(Component component, Graphics graphics, int x, int y, int width, int height)
     {
-        insets.set(4, 8, 4, 8);
+        Graphics2D graphics2d = (Graphics2D)graphics.create();
+        graphics2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        Shape border = getBorderShape(x, y, width - 1, height - 1);
+        graphics2d.setPaint(ALPHA_ZERO);
+        Area corner = new Area(new Rectangle2D.Double(x, y, width, height));
+        corner.subtract(new Area(border));
+        graphics2d.fillRoundRect(x, y, width - 1, height - 1, 5, 5);
+        graphics2d.setPaint(_color);
+        graphics2d.drawRoundRect(x, y, width - 1, height - 1, 5, 5);
+        graphics2d.dispose();
+    }
+    
+    public Shape getBorderShape(int x, int y, int width, int height) 
+    {
+        return new RoundRectangle2D.Double(x, y, width, height, height, height);
+    }
+    
+    @Override 
+    public Insets getBorderInsets(Component component)
+    {
+        return BORDER_INSETS;
+    }
+
+    @Override
+    public Insets getBorderInsets(Component component, Insets insets) 
+    {
+        insets.set(TOP, LEFT, BOTTOM, RIGHT);
         return insets;
     }
 }
